4bd6d7193fa4912df21c1a4f5a97bf122ab70296
DaedalusHatak <pawelkic55@gmail.com>
2023-10-05 23:01:47 +0200

    feat: working carousel animation/movement

--- app/browse/page.tsx
+++ app/browse/page.tsx
@@ -5,44 +5,46 @@ import { cookies } from "next/headers";
 import Carousel from "./carousel";
 async function getData() {
   try {
-    if(!cookies().get("name")) return "Raiden.webp";
+    if (!cookies().get("name")) return "Raiden.webp";
     const user = await firebaseAdmin
       .auth()
       .verifyIdToken(cookies().get("name")!.value);
     return user.picture ? user.picture : "Raiden.webp";
   } catch (e) {
-    return "Raiden.webp"
+    return "Raiden.webp";
   }
 }
 export default async function Page() {
   const data = await getData();
   const movie = await fetch(
     `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.NEXT_API_SECRET}`
-  )
+  );
   const res = await movie.json();
-  console.log(res)
+  console.log(res);
   return (
     <>
       <NavBar user={data}></NavBar>
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <main className="flex min-h-screen flex-col items-center justify-between p-[3rem]">
         <div className="flex flex-col gap-2">
           {" "}
           <h1>Hello world</h1>
-          {res && <Carousel data={res.results}  
-             emitImage={true}
-             
-             emitImageDelay={350}
-    size={2}
-    sm={3}
-    md={4}
-    lg={5}
-    overflow="true"
-    buttonPadding="-3"
-    buttonWidth="3"
-    buttonVisibile="true"
-    xl={6}
-    xxl={7} />}
-
+          {res && (
+            <Carousel
+              data={res.results}
+              emitImage={true}
+              emitImageDelay={350}
+              size={2}
+              sm={3}
+              md={4}
+              lg={5}
+              overflow={true}
+              buttonPadding={-3}
+              buttonWidth={3}
+              buttonVisibile={true}
+              xl={6}
+              xxl={7}
+            />
+          )}
         </div>
       </main>
     </>
--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [{ protocol: "https", hostname: "image.tmdb.org" }],
+  },
   env: {
     firebaseAdmin: {
       projectId: process.env.FIREBASE_SA_PROJECT_ID,
