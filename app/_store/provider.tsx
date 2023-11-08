004f188cf3eee6af4a8ab40aa637f0e33bfd38ef
DaedalusHatak <pawelkic55@gmail.com>
2023-11-08 12:31:08 +0100

    feat: page loader

--- app/_store/provider.tsx
+++ app/_store/provider.tsx
@@ -1,8 +1,13 @@
 "use client";
 
+import { Next13ProgressBar } from "next13-progressbar";
 import { store } from "./index";
 import { Provider } from "react-redux";
 
 export function ReduxProvider({ children }: { children: React.ReactNode }) {
-  return <Provider store={store}>{children}</Provider>;
+  return <>
+  <Provider store={store}>{children}</Provider>
+  
+  <Next13ProgressBar height="4px" color="#0A2FFF" showOnShallow />
+  </>;
 }
